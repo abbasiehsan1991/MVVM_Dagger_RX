efa580f03a8d10d2163ee0324023567174b5f5f0
Ehsan Abbasi <abbasi.ehsan1991@gmail.com>
2018-12-08 15:47:00 +0330

    fixed some bugs

--- app/src/main/java/codenevisha/ir/mvvmwithdagger/ui/base/BaseBindings.kt
+++ app/src/main/java/codenevisha/ir/mvvmwithdagger/ui/base/BaseBindings.kt
@@ -3,7 +3,13 @@ package codenevisha.ir.mvvmwithdagger.ui.base
 import android.databinding.BindingAdapter
 import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.widget.ImageView
+import codenevisha.ir.mvvmwithdagger.R
 import codenevisha.ir.mvvmwithdagger.ui.AdapterHome
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 
 
 /**
@@ -26,6 +32,26 @@ class BaseBindings {
                     (recyclerView.adapter as AdapterHome).swapData(items)
                 }
 
+            }
+        }
+
+        @JvmStatic
+        @BindingAdapter("app:loadImage")
+        fun setImageResource(view: ImageView, imageUrl: String?) {
+
+            val context = view.context
+
+            val options = RequestOptions()
+                .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+
+
+            imageUrl?.let {
+
+                Glide.with(context)
+                    .setDefaultRequestOptions(options)
+                    .load(imageUrl)
+                    .transition(DrawableTransitionOptions.withCrossFade(1000))
+                    .into(view)
             }
         }
     }
